0f384eb385a284d785b4600e1f12595be14b439f
rodion.suprun <rodion.suprun@homecredit.kz>
2023-06-21 17:35:10 +0600

    Added fields error

--- lib/presentation/login/bloc/login_state.dart
+++ lib/presentation/login/bloc/login_state.dart
@@ -2,26 +2,39 @@ part of 'login_bloc.dart';
 
 class LoginState extends Equatable {
   final String email;
+  final EmailError emailError;
   final String password;
+  final PasswordError passwordError;
   final bool emailValid;
   final bool passwordValid;
   final bool authenticated;
 
   const LoginState({
     required this.email,
+    required this.emailError,
     required this.password,
+    required this.passwordError,
     required this.emailValid,
     required this.passwordValid,
     required this.authenticated,
   });
 
   @override
-  List<Object?> get props =>
-      [email, password, emailValid, passwordValid, authenticated];
+  List<Object?> get props => [
+        email,
+        emailError,
+        password,
+        passwordError,
+        emailValid,
+        passwordValid,
+        authenticated
+      ];
 
   factory LoginState.initial() => const LoginState(
         email: "",
+        emailError: EmailError.noError,
         password: "",
+        passwordError: PasswordError.noError,
         emailValid: false,
         passwordValid: false,
         authenticated: false,
@@ -29,14 +42,18 @@ class LoginState extends Equatable {
 
   LoginState copyWith({
     final String? email,
+    final EmailError? emailError,
     final String? password,
+    final PasswordError? passwordError,
     final bool? emailValid,
     final bool? passwordValid,
     final bool? authenticated,
   }) {
     return LoginState(
       email: email ?? this.email,
+      emailError: emailError ?? this.emailError,
       password: password ?? this.password,
+      passwordError: passwordError ?? this.passwordError,
       emailValid: emailValid ?? this.emailValid,
       passwordValid: passwordValid ?? this.passwordValid,
       authenticated: authenticated ?? this.authenticated,
