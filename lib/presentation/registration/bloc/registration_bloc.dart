b7e7721a04b7fa3feba3c794f2962348e17fd1d2
rodion.suprun <rodion.suprun@homecredit.kz>
2023-06-27 10:36:44 +0600

    Added registration bloc

---
+++ lib/presentation/registration/bloc/registration_bloc.dart
@@ -0,0 +1,16 @@
+import 'dart:async';
+
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:meta/meta.dart';
+
+part 'registration_event.dart';
+part 'registration_state.dart';
+
+class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
+  RegistrationBloc() : super(RegistrationInitial()) {
+    on<RegistrationEvent>((event, emit) {
+      // TODO: implement event handler
+    });
+  }
+}
