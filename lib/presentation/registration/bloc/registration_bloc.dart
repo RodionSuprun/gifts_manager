b92880b3a9365b99f81d399308be7a4e97c10b14
rodion.suprun <rodion.suprun@homecredit.kz>
2023-06-27 13:18:19 +0600

    Added all text fields

--- lib/presentation/registration/bloc/registration_bloc.dart
+++ lib/presentation/registration/bloc/registration_bloc.dart
@@ -2,6 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:bloc/bloc.dart';
+import 'package:email_validator/email_validator.dart';
 import 'package:equatable/equatable.dart';
 import 'package:gifts_manager/data/model/request_error.dart';
 import 'package:gifts_manager/presentation/registration/model/errors.dart';
@@ -19,10 +20,28 @@ class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
 
   String _avatarKey = defaultAvatarKey;
 
+  String _email = "";
+  bool _highlightEmailError = false;
+  RegistrationEmailError? _emailError;
+
+  String _password = "";
+  bool _highlightPasswordError = false;
+  RegistrationPasswordError? _passwordError;
+
   RegistrationBloc()
       : super(RegistrationFieldsInfo(
             avatarLink: _avatarBuilder(defaultAvatarKey))) {
     on<RegistrationChangeAvatar>(_onChangeAvatar);
+    on<RegistrationEmailChanged>(_onChangeEmail);
+    on<RegistrationEmailFocusLost>(_onEmailFocusLost);
+  }
+
+  FutureOr<void> _onEmailFocusLost(
+    final RegistrationEmailFocusLost event,
+    final Emitter<RegistrationState> emit,
+  ) {
+    _highlightEmailError = true;
+    emit(_calculateFieldsInfo());
   }
 
   FutureOr<void> _onChangeAvatar(
@@ -30,6 +49,33 @@ class RegistrationBloc extends Bloc<RegistrationEvent, RegistrationState> {
     final Emitter<RegistrationState> emit,
   ) {
     _avatarKey = Random().nextInt(1000000).toString();
-    emit(RegistrationFieldsInfo(avatarLink: _avatarBuilder(_avatarKey)));
+    emit(_calculateFieldsInfo());
+  }
+
+  FutureOr<void> _onChangeEmail(
+    final RegistrationEmailChanged event,
+    final Emitter<RegistrationState> emit,
+  ) {
+    _email = event.email;
+    _emailError = _validateEmail();
+    emit(_calculateFieldsInfo());
+  }
+
+  RegistrationFieldsInfo _calculateFieldsInfo() {
+    return RegistrationFieldsInfo(
+      avatarLink: _avatarBuilder(_avatarKey),
+      emailError: _highlightEmailError ? _emailError : null,
+      passwordError: _passwordError,
+    );
+  }
+
+  RegistrationEmailError? _validateEmail() {
+    if (_email.isEmpty) {
+      return RegistrationEmailError.empty;
+    }
+    if (!EmailValidator.validate(_email)) {
+      return RegistrationEmailError.invalid;
+    }
+    return null;
   }
 }
--- lib/presentation/registration/view/registration_page.dart
+++ lib/presentation/registration/view/registration_page.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:gifts_manager/extensions/build_context.dart';
 import 'package:gifts_manager/extensions/theme_extension.dart';
 import 'package:gifts_manager/presentation/registration/bloc/registration_bloc.dart';
+import 'package:gifts_manager/presentation/registration/model/errors.dart';
 import 'package:gifts_manager/resources/app_colors.dart';
 
 class RegistrationPage extends StatelessWidget {
@@ -29,6 +31,61 @@ class _RegistrationPageWidget extends StatefulWidget {
 }
 
 class _RegistrationPageWidgetState extends State<_RegistrationPageWidget> {
+  late final FocusNode _emailFocusNode;
+  late final FocusNode _passwordFocusNode;
+  late final FocusNode _passwordConfirmationFocusNode;
+  late final FocusNode _nameFocusNode;
+
+  @override
+  void initState() {
+    super.initState();
+    _emailFocusNode = FocusNode();
+    _passwordFocusNode = FocusNode();
+    _passwordConfirmationFocusNode = FocusNode();
+    _nameFocusNode = FocusNode();
+
+    SchedulerBinding.instance
+        .addPostFrameCallback((_) => _addFocusLostHandlers());
+  }
+
+  void _addFocusLostHandlers() {
+    _emailFocusNode.addListener(() {
+      if (!_emailFocusNode.hasFocus) {
+        context
+            .read<RegistrationBloc>()
+            .add(const RegistrationEmailFocusLost());
+      }
+    });
+    _passwordFocusNode.addListener(() {
+      if (!_passwordFocusNode.hasFocus) {
+        context
+            .read<RegistrationBloc>()
+            .add(const RegistrationPasswordFocusLost());
+      }
+    });
+    _passwordConfirmationFocusNode.addListener(() {
+      if (!_passwordConfirmationFocusNode.hasFocus) {
+        context
+            .read<RegistrationBloc>()
+            .add(const RegistrationPasswordConfirmationFocusLost());
+      }
+    });
+    _nameFocusNode.addListener(() {
+      if (!_nameFocusNode.hasFocus) {
+        context.read<RegistrationBloc>().add(const RegistrationNameFocusLost());
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    _emailFocusNode.dispose();
+    _passwordFocusNode.dispose();
+    _passwordConfirmationFocusNode.dispose();
+    _nameFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -39,13 +96,214 @@ class _RegistrationPageWidgetState extends State<_RegistrationPageWidget> {
             Expanded(
               child: ListView(
                 children: [
-                  _AvatarWidget(),
+                  const SizedBox(height: 8),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 16),
+                    child: Text(
+                      "Создать аккаунт",
+                      style: context.theme.h2,
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  _EmailTextField(
+                    emailFocusNode: _emailFocusNode,
+                    passwordFocusNode: _passwordFocusNode,
+                  ),
+                  _PasswordTextField(
+                    passwordFocusNode: _passwordFocusNode,
+                    passwordConfirmationFocusNode:
+                        _passwordConfirmationFocusNode,
+                  ),
+                  _PasswordConfirmationTextField(
+                    passwordConfirmationFocusNode:
+                        _passwordConfirmationFocusNode,
+                    nameFocusNode: _nameFocusNode,
+                  ),
+                  _NameTextField(nameFocusNode: _nameFocusNode),
+                  const SizedBox(height: 16),
+                  const _AvatarWidget(),
                 ],
               ),
             ),
-            _RegistrationButton(),
+            const _RegistrationButton(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class _NameTextField extends StatelessWidget {
+  const _NameTextField({
+    super.key,
+    required FocusNode nameFocusNode,
+  }) : _nameFocusNode = nameFocusNode;
+
+  final FocusNode _nameFocusNode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: BlocBuilder<RegistrationBloc, RegistrationState>(
+        buildWhen: (_, current) {
+          return current is RegistrationFieldsInfo;
+        },
+        builder: (context, state) {
+          final fieldsInfo = state as RegistrationFieldsInfo;
+          final error = fieldsInfo.nameError;
+          return TextField(
+            focusNode: _nameFocusNode,
+            autocorrect: false,
+            onChanged: (text) {
+              context
+                  .read<RegistrationBloc>()
+                  .add(RegistrationNameChanged(name: text));
+            },
+            onSubmitted: (text) {
+              FocusManager.instance.primaryFocus?.unfocus();
+            },
+            decoration: InputDecoration(
+              labelText: "Имя и фамилия",
+              errorText: error?.toString(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _PasswordConfirmationTextField extends StatelessWidget {
+  const _PasswordConfirmationTextField({
+    super.key,
+    required FocusNode passwordConfirmationFocusNode,
+    required FocusNode nameFocusNode,
+  })  : _passwordConfirmationFocusNode = passwordConfirmationFocusNode,
+        _nameFocusNode = nameFocusNode;
+
+  final FocusNode _passwordConfirmationFocusNode;
+  final FocusNode _nameFocusNode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: BlocBuilder<RegistrationBloc, RegistrationState>(
+        buildWhen: (_, current) {
+          return current is RegistrationFieldsInfo;
+        },
+        builder: (context, state) {
+          final fieldsInfo = state as RegistrationFieldsInfo;
+          final error = fieldsInfo.passwordConfirmationError;
+          return TextField(
+            focusNode: _passwordConfirmationFocusNode,
+            autocorrect: false,
+            obscureText: true,
+            onChanged: (text) {
+              context.read<RegistrationBloc>().add(
+                  RegistrationPasswordConfirmationChanged(
+                      passwordConfirmation: text));
+            },
+            onSubmitted: (text) {
+              _nameFocusNode.requestFocus();
+            },
+            decoration: InputDecoration(
+              labelText: "Пароль второй раз",
+              errorText: error?.toString(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _PasswordTextField extends StatelessWidget {
+  const _PasswordTextField({
+    super.key,
+    required FocusNode passwordFocusNode,
+    required FocusNode passwordConfirmationFocusNode,
+  })  : _passwordFocusNode = passwordFocusNode,
+        _passwordConfirmationFocusNode = passwordConfirmationFocusNode;
+
+  final FocusNode _passwordFocusNode;
+  final FocusNode _passwordConfirmationFocusNode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: BlocBuilder<RegistrationBloc, RegistrationState>(
+        buildWhen: (_, current) {
+          return current is RegistrationFieldsInfo;
+        },
+        builder: (context, state) {
+          final fieldsInfo = state as RegistrationFieldsInfo;
+          final error = fieldsInfo.passwordError;
+          return TextField(
+            focusNode: _passwordFocusNode,
+            autocorrect: false,
+            obscureText: true,
+            onChanged: (text) {
+              context
+                  .read<RegistrationBloc>()
+                  .add(RegistrationPasswordChanged(password: text));
+            },
+            onSubmitted: (text) {
+              _passwordConfirmationFocusNode.requestFocus();
+            },
+            decoration: InputDecoration(
+              labelText: "Пароль",
+              errorText: error?.toString(),
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class _EmailTextField extends StatelessWidget {
+  const _EmailTextField({
+    super.key,
+    required FocusNode emailFocusNode,
+    required FocusNode passwordFocusNode,
+  })  : _emailFocusNode = emailFocusNode,
+        _passwordFocusNode = passwordFocusNode;
+
+  final FocusNode _emailFocusNode;
+  final FocusNode _passwordFocusNode;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
+      child: BlocBuilder<RegistrationBloc, RegistrationState>(
+        buildWhen: (_, current) {
+          return current is RegistrationFieldsInfo;
+        },
+        builder: (context, state) {
+          final fieldsInfo = state as RegistrationFieldsInfo;
+          final error = fieldsInfo.emailError;
+          return TextField(
+            focusNode: _emailFocusNode,
+            autocorrect: false,
+            keyboardType: TextInputType.emailAddress,
+            onChanged: (text) {
+              context
+                  .read<RegistrationBloc>()
+                  .add(RegistrationEmailChanged(email: text));
+            },
+            onSubmitted: (text) {
+              _passwordFocusNode.requestFocus();
+            },
+            decoration: InputDecoration(
+              labelText: "Почта",
+              errorText: error?.toString(),
+            ),
+          );
+        },
       ),
     );
   }
@@ -100,7 +358,7 @@ class _AvatarWidget extends StatelessWidget {
                     const RegistrationChangeAvatar(),
                   );
             },
-            child: Text("Изменить"),
+            child: const Text("Изменить"),
           ),
         ],
       ),
